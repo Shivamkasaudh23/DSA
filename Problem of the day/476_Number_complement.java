ca349b0dcbeea9752a6c2d62a2e47139da605147
ShivamKasaudh23 <shivamkasaudh23@gmail.com>
2024-08-23 00:22:49 +0530

    476 Number Complement

---
+++ Problem of the day/476_Number_complement.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findComplement(int num) {
+        int numBits = (int)(Math.log(num) / Math.log(2)) + 1;
+
+        int mask = (1 << numBits) - 1;
+
+        return num^mask;
+    }
+}
